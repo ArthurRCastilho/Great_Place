a70b73564c5778d4aff1e6d0e379bf59ad213ec7
ArthurLVidal <arthurodri2017@gmail.com>
2024-06-22 12:20:46 -0300

    Escondendo API

--- lib/utils/location_util.dart
+++ lib/utils/location_util.dart
@@ -1,10 +1,12 @@
-const GOOGLE_API_KEY = 'AIzaSyBJGxCR_hFSK_hBxcHLR6iNeNf5fvonBWI';
+import './google_api_util.dart';
 
 class LocationUtil {
   static String generateLocationPreviewImage({
     required double latitude,
     required double logitude,
   }) {
-    return 'https://maps.googleapis.com/maps/api/staticmap?center=$latitude,$logitude&zoom=13&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$latitude,$logitude&key=$GOOGLE_API_KEY';
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=$latitude,$logitude&zoom=13&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$latitude,$logitude&key=${GoogleApiUtil.GOOGLE_API_KEY}';
   }
+
+  // A API foi escondida :)
 }
